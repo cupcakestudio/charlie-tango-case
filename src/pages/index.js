f87bbabc3effadb719c45d9238ba100d295a306a
Simone <ssimone12@gmail.com>
2023-04-27 12:08:03 +0200

    link til næste virker

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,27 @@
 import Head from "next/head";
-import styles from "./Home.module.css";
-import { Landingpage } from "@/components/Header/landingpage";
-import FormCard from "@/components/FormCard";
 
+import { Landingpage } from "@/components/Landingpage";
+import FormCard from "@/components/FormCard";
+import { useState } from "react";
 export default function Home() {
+  /* konstanten page tjekker hvilken side vi er på med useState */
+  const [page, setPage] = useState("landing");
   return (
     <>
       <Head>
         <title>Find buyer | EDC</title>
       </Head>
-      <section>
-        <Landingpage></Landingpage>
-      </section>
-      <div className="wrapper">
-        <FormCard />
-      </div>
+      {/* definere en if sætning der sætter pagen til landing som standart og bruger null til at gemme alt der ikke er = landing */}
+      {page === "landing" ? (
+        <Landingpage setPage={setPage}></Landingpage>
+      ) : null}
+
+      {/* definere en state til denne side*/}
+      {page === "yourEstate" ? (
+        <div className="wrapper">
+          <FormCard />
+        </div>
+      ) : null}
     </>
   );
 }
