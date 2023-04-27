07ebc18b5848b7c03c5ec3abc8cced37d1ee14a0
ellen_hein <elle7227@stud.kea.dk>
2023-04-27 10:30:48 +0200

    thank you form - mangler styling

---
+++ src/components/FormthankYou.jsx
@@ -0,0 +1,15 @@
+import styles from "src/pages/Home.module.css";
+
+export function FormthankYou() {
+  return (
+    <>
+    <section className="container">
+      <article className={styles.formBackground}>
+      <h1 className={styles.headline}>Thnak you</h1>
+          <p>We will be in touch within 2-3 working days</p>
+          <a href="#">Back to EDC &#8594;</a>
+      </article>
+    </section>
+    </>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "./Home.module.css";
 import { Landingpage } from "@/components/Header/landingpage";
 import FormCard from "@/components/FormCard";
+import { FormthankYou } from "@/components/FormthankYou";
 
 export default function Home() {
   return (
@@ -13,8 +14,11 @@ export default function Home() {
         <Landingpage></Landingpage>
       </section>
       <div className="wrapper">
-        <FormCard />
+        <FormCard></FormCard>
       </div>
+      <section>
+        <FormthankYou></FormthankYou>
+      </section>
     </>
   );
 }
