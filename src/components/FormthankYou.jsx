bcb50e953f313ac2486a2eb96d955d5ab487e652
ellen_hein <elle7227@stud.kea.dk>
2023-04-27 12:09:40 +0200

    thank you side

--- src/components/FormthankYou.jsx
+++ src/components/FormthankYou.jsx
@@ -3,13 +3,11 @@ import styles from "src/pages/Home.module.css";
 export function FormthankYou() {
   return (
     <>
-    <section className="container">
       <article className={styles.formBackground}>
       <h1 className={styles.headline}>Thnak you</h1>
           <p>We will be in touch within 2-3 working days</p>
           <a href="#">Back to EDC &#8594;</a>
       </article>
-    </section>
     </>
   );
 }
