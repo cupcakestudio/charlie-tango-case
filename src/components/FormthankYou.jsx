cbf1bbb41aaf726d443ab9c72598d8c87f8cba39
ellen_hein <elle7227@stud.kea.dk>
2023-04-27 12:10:06 +0200

    thank you side

--- src/components/FormthankYou.jsx
+++ src/components/FormthankYou.jsx
@@ -4,10 +4,15 @@ export function FormthankYou() {
   return (
     <>
       <article className={styles.formBackground}>
+        <section className="thankYouSection">
       <h1 className={styles.headline}>Thnak you</h1>
-          <p>We will be in touch within 2-3 working days</p>
-          <a href="#">Back to EDC &#8594;</a>
+          <p className="thankYouText">We will be in touch within 2-3 working days</p>
+          <button className="thankYouLink">
+            <a className="a_link" href="#">Back to EDC &#8594;</a>
+          </button>
+          </section>
       </article>
+    
     </>
   );
 }
