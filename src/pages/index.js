8de29a35fec77d3b95a87e10db4b5df6b9999fab
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-04-27 13:17:11 +0200

    potential style virker

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "./Home.module.css";
-import { Landingpage } from "@/components/Header/landingpage";
+import { Landingpage } from "@/components/Landingpage";
 import FormCard from "@/components/FormCard";
 import { FormthankYou } from "@/components/FormthankYou";
 import { useState } from "react";
@@ -12,26 +12,14 @@ export default function Home() {
       <Head>
         <title>Find buyer | EDC</title>
       </Head>
-      <section>
-        <Landingpage></Landingpage>
-      </section>
-      <div className="wrapper">
-        <FormCard></FormCard>
-      </div>
-      <section>
-        <FormthankYou></FormthankYou>
-      </section>
+
       {/* definere en if sætning der sætter pagen til landing som standart og bruger null til at gemme alt der ikke er = landing */}
       {page === "landing" ? (
         <Landingpage setPage={setPage}></Landingpage>
       ) : null}
 
       {/* definere en state til denne side*/}
-      {page === "yourEstate" ? (
-        <div className="wrapper">
-          <FormCard />
-        </div>
-      ) : null}
+      {page === "yourEstate" ? <FormCard /> : null}
     </>
   );
 }
