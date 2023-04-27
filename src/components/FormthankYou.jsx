e4e7a44a431dd4f1a5c1a924260254fc4b3fe2ae
ellen_hein <elle7227@stud.kea.dk>
2023-04-27 13:18:17 +0200

    lige gyldigt

--- src/components/FormthankYou.jsx
+++ src/components/FormthankYou.jsx
@@ -3,15 +3,17 @@ import styles from "src/pages/Home.module.css";
 export function FormthankYou() {
   return (
     <>
-      <article className={styles.formBackground}>
         <section className="thankYouSection">
+      <article className={styles.formBackground}>
+    
       <h1 className={styles.headline}>Thnak you</h1>
           <p className="thankYouText">We will be in touch within 2-3 working days</p>
           <button className="thankYouLink">
             <a className="a_link" href="#">Back to EDC &#8594;</a>
           </button>
-          </section>
+        
       </article>
+      </section>
     
     </>
   );
