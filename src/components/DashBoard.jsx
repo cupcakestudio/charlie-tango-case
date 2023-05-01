9d47f9bb16fcc1e5de05192511331c02fcb23fe4
Ting Hua Lin <coolting.lin3@gmail.com>
2023-05-01 15:11:07 +0200

    add endpoint for db, not working correctly

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,6 +1,19 @@
 import styles from "src/pages/Home.module.css";
 import { DashCard } from "src/components/DashCard.jsx";
-export function DashBoard() {
+import { useState, useEffect } from "react";
+export default function DashBoard() {
+const [SellerEDCContact, setSellerEDCContact] = useState();
+  useEffect(() => {
+    fetch(
+      // fetcher api med props og læser state data (sellerData)
+      'https://gdbpieovhxbltmbhyctf.supabase.co/rest/v1/sellers?select=id'
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // setpotentialBuyer(data);
+        console.log(data);
+      });
+  }, []);
   return (
     <>
       <h1 className={styles.headline}>Latest contacts</h1>
---
+++ src/pages/dashboard.js
@@ -0,0 +1,7 @@
+import DashBoard from "@/components/DashBoard";
+import { DashCard } from "@/components/DashCard";
+import styles from "src/pages/Home.module.css";
+
+export default function DashboardEDC() {
+  return <DashBoard></DashBoard>;
+}
