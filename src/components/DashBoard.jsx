6e374d8daf25e9c6bb8423d529743d0600c01e73
Ting Hua Lin <coolting.lin3@gmail.com>
2023-05-02 11:07:10 +0200

    data can be loaded from supabase

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -2,18 +2,25 @@ import styles from "src/pages/Home.module.css";
 import { DashCard } from "src/components/DashCard.jsx";
 import { useState, useEffect } from "react";
 export default function DashBoard() {
-const [SellerEDCContact, setSellerEDCContact] = useState();
-  useEffect(() => {
-    fetch(
+
+  const [SellerEDCContact, setSellerEDCContact] = useState(null);
+ useEffect(() => {
+
       // fetcher api med props og læser state data (sellerData)
-      'https://gdbpieovhxbltmbhyctf.supabase.co/rest/v1/sellers?select=id'
-    )
+       fetch('/api/getSupabaseData', {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+       })
       .then((res) => res.json())
       .then((data) => {
-        // setpotentialBuyer(data);
+        // setSellerEDCContact(data);
         console.log(data);
-      });
-  }, []);
+      })
+  }, []
+  )
+
   return (
     <>
       <h1 className={styles.headline}>Latest contacts</h1>
---
+++ src/pages/api/getSupabaseData.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  // fetcher api med props og læser state data (sellerData)
+  fetch("https://gdbpieovhxbltmbhyctf.supabase.co/rest/v1/sellers", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      apikey: process.env.SUPABASE_KEY,
+      Prefer: "return=representation",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("data from DB ", data);
+      res.status(200).json({ data });
+    });
+}
