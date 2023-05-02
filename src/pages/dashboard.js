ae590fa808a7253d1b7649ea300986b3cc794a38
ellen_hein <elle7227@stud.kea.dk>
2023-05-02 10:51:41 +0200

    dashboard styling pt.1

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,22 @@
-import { DashBoard } from "@/components/DashBoard";
 import { DashCard } from "@/components/DashCard";
 import styles from "src/pages/Home.module.css";
 
-export default function DashboardEDC() {
-  return <DashBoard></DashBoard>;
+export default function Dashboard() {
+  return (
+    <>
+      <h1 className={styles.headline}>Latest contacts</h1>
+      <div className={styles.potentialWrapper}>
+        <section className={styles.formBackground}>
+          <div className={styles.potential_container}>
+            <p>amount of contacts: X</p>
+            <div className={styles.potential_grid}>
+              <div className={styles.potential_cards}>
+                <DashCard />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
