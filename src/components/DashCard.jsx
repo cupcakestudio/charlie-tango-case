7c8036650cf8e4c78ee93634184dd1db3c05d8e2
Simone <ssimone12@gmail.com>
2023-05-03 09:27:03 +0200

    recieved at

--- src/components/DashCard.jsx
+++ src/components/DashCard.jsx
@@ -1,37 +1,55 @@
 import styles from "src/pages/Home.module.css";
 import { useEffect } from "react";
 export function DashCard(props) {
-  console.log("buyers  " ,props.buyers)
+  console.log("buyers  ", props.buyers);
   return (
     <>
       <section className={styles.card}>
         <div className={styles.date_delete}>
-          <p>Date: {props.inserted_at}</p>
+          <p>Date: {props.received_at}</p>
           <button>Delete</button>
         </div>
 
         <div className={styles.divContacts}>
-            <div className="contactsLeft">
-                <p>Name: {props.fullname}</p>
-                <p>E-mail: <span><a href="mailto:{props.email}">{props.email}</a></span></p>
-                <p>Phone: <span><a href="tel:{props.phone}">{props.phone}</a></span></p>
-                {/* if seller has checked "#consent checkbox" display yes, else no */}
-                <p>Consent to be contacted:  <span>{props.consent ? "YES": "NO"}</span></p>
+          <div className="contactsLeft">
+            <p>Name: {props.fullname}</p>
+            <p>
+              E-mail:{" "}
+              <span>
+                <a href="mailto:{props.email}">{props.email}</a>
+              </span>
+            </p>
+            <p>
+              Phone:{" "}
+              <span>
+                <a href="tel:{props.phone}">{props.phone}</a>
+              </span>
+            </p>
+            {/* if seller has checked "#consent checkbox" display yes, else no */}
+            <p>
+              Consent to be contacted:{" "}
+              <span>{props.consent ? "YES" : "NO"}</span>
+            </p>
+          </div>
 
-            </div>
-       
-            <div className="contactsright">
-                <p>Estate: {props.estateType}</p>
-                <p>Price: {props.price}</p>
-                <p>Size: {props.size}</p>
-                <p>Zip code: {props.zipCode}</p>
-                <p>Selected Potenial Buyers </p> <ul> {props.buyers.map(obj =><li key={obj.id}>{obj.id}</li>)}</ul>
-            </div>
+          <div className="contactsright">
+            <p>Estate: {props.estateType}</p>
+            <p>Price: {props.price}</p>
+            <p>Size: {props.size}</p>
+            <p>Zip code: {props.zipCode}</p>
+            <p>Selected Potenial Buyers </p>{" "}
+            <ul>
+              {" "}
+              {props.buyers.map((obj) => (
+                <li key={obj.id}>{obj.id}</li>
+              ))}
+            </ul>
+          </div>
         </div>
 
         <div className={styles.contactedCheckBox}>
-            <label for="contacted">Contacted</label>
-            <input type="checkbox" id="contacted" name="contacted" required />
+          <label for="contacted">Contacted</label>
+          <input type="checkbox" id="contacted" name="contacted" required />
         </div>
       </section>
     </>
