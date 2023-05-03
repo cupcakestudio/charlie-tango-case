5c917bbefb9c1af967192982ae54215dea7d6c09
ellen_hein <elle7227@stud.kea.dk>
2023-05-03 13:10:40 +0200

    back knap

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,9 +52,21 @@ export default function Home() {
       {page !== "landing" && <StepBar activeStep={getActive()} />}
       {page !== "landing" && (
         <div className={styles.back_next_buttons}>
-          <button className={styles.button_back_next} onClick={handleBack}>
-            Back
-          </button>
+          <div className={styles.back_next} onClick={handleBack}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              class="bi bi-arrow-left-circle"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+              />
+            </svg>{" "}
+          </div>
         </div>
       )}
       {page === "landing" ? <Landingpage setPage={setPage} /> : null}
